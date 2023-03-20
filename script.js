d92b8b26e07f90281f3ac5c8a2be535ec977eab3
EmadNova <im.nova@yahoo.com>
2023-03-20 21:12:01 +0330

    all things fixed

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
 const burger = document.querySelector(".burger_");
 const navMenu = document.querySelector(".navMenu");
+const social = document.querySelector(".social_");
+const close = document.querySelector(".close_");
 
 burger.addEventListener("click", () => {
-    burger.classList.toggle(".active")
-    navMenu.classList.toggle(".active")
+    burger.classList.toggle("active")
+    navMenu.classList.toggle("active")
+    social.classList.toggle("active")
+    close.classList.toggle("active")
+})
+
+document.querySelectorAll("li").forEach(n => n.addEventListener("click", () => {
+    burger.classList.remove("active")
+    navMenu.classList.remove("active")
+    social.classList.remove("active")
+}))
+
+close.addEventListener("click", () => {
+    navMenu.classList.remove("active")
+    social.classList.remove("active")
 })
